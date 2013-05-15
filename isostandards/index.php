237460aa9f2cba279cb03aea851cb7f550053e6a
Timothy Cool <tcoolpg@gmail.com>
2013-05-14 19:30:46 -0700

    Made some changes to the index page

--- isostandards/index.php
+++ isostandards/index.php
@@ -7,20 +7,28 @@ include("Global.php");
 <!DOCTYPE HTML>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-wysihtml5.css"></link>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"></link>
+		<script src="js/wysihtml5-0.3.0.js"></script>
+		<script src="js/jquery-1.7.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/bootstrap-wysihtml5.js"></script>
 	<?php 
-		App::loadCSS('bootstrap.min');
+		//App::loadCSS('bootstrap.min');
 		App::loadCSS('bootstrap-editable');
-		App::loadCSS('wysihtml5');
+		//App::loadCSS('wysihtml5');
 		App::loadCSS('app');
 		App::loadCSS('menu');
-		App::loadScript('jquery-1.9.1.min');
-		App::loadScript('bootstrap.min');
+		//App::loadScript('jquery-1.7.2.min');
+		//App::loadScript('bootstrap.min');
 		App::loadScript('bootstrap-editable.min');
-		App::loadScript('bootstrap-wysihtml5');
+		//App::loadScript('bootstrap-wysihtml5');
 		App::loadScript('wysihtml5');
 		App::loadScript('app');
 		App::loadScript('menu');
+
 	?>
+		
 	</head>
 	<body>
 	<?php
@@ -33,15 +41,17 @@ include("Global.php");
 					'Documents' => array('mode'=>'dropdown'),
 					'Training',
 					'Purchase',
-					'Menu',
-					'Articles' => array('mode'=>'dropdown'),
 					'Design');
 		App::displayMenuBar("ISO",$menuItems);
 
 		//Create contents
 		$containers = array('articles-side-bar' => 3,'article-content' => 9);
 		App::displayContent($containers);
+
+		//$fields = array('name'=>'Tim','age'=>26,'document'=>'Test Doc');
+		//$db = Database::get()->insert('test table',$fields);
 	?>
+</script>
 	</body>
 	<footer>
 
